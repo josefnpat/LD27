704ac85f9fab0cc7f1bed91dd98699ce06b3532c
Josef Patoprsty <seppi@josefnpat.com>
2013-08-25 15:16:34 -0400

    updating fonts for 800x800 default

--- src/fonts.lua
+++ src/fonts.lua
@@ -1,6 +1,6 @@
 local fonts = {}
 
-fonts.large = love.graphics.newFont("assets/Coalition_v2.ttf",48)
+fonts.large = love.graphics.newFont("assets/Coalition_v2.ttf",40)
 fonts.small = love.graphics.newFont("assets/NewRocker-Regular.ttf",16)
 
 return fonts
